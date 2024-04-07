9ab9cbad1d2122da5cf5f5c9156ed5dc3148b785
Shintaro Kojima <goodies@codeout.net>
2024-04-07 12:10:30 +0900

    chore: configure pre-commit and rubocop

---
+++ commitlint.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    // "scope-case": [1, "always"],
+    // "subject-case": [1, "always"],
+    // "body-max-line-length": [1, "always", 100],
+  },
+};
